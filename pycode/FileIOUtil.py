c3d79d9af6cb8345adc2de1d30ed37e94d127445
Akshathkumar Shetty <akshath@gmail.com>
2021-10-20 23:39:47 +0530

    improved

--- pycode/FileIOUtil.py
+++ pycode/FileIOUtil.py
@@ -4,14 +4,17 @@ import os
 
 def get_dir(path_loc, only_dir=True):
     result = []
-    for f in listdir(path_loc):
-        if f.startswith('.') or f.startswith('__'): #skip hidden folders
-            continue
-        if only_dir:
-            if isdir(join(path_loc, f)):
+    try:
+        for f in listdir(path_loc):
+            if f.startswith('.') or f.startswith('__'): #skip hidden folders
+                continue
+            if only_dir:
+                if isdir(join(path_loc, f)):
+                    result.append(f)
+            else:
                 result.append(f)
-        else:
-            result.append(f)
+    except FileNotFoundError as fe:
+        print('Error: ',fe)
     return result
     
 def print_dir(path_loc, only_dir=True):
